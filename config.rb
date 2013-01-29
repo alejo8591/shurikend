b7487801ccf81a74520155408b2b3b8bb2b3e71c
Alejandro Romero <alejo8591@gmail.com>
2013-01-29 01:43:46 -0500

    code migration and update

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@
 require 'modular-scale'
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "test/vendor/assets/css"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
